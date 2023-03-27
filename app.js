4e91ecc69491d454aa1da3507cfcb993350106f1
Nasif Noor Saleheen <nasif.arnob1@gmail.com>
2023-03-27 23:23:15 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+
+const https = require("https");
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res){
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+      }
+
+    ]
+  }
+
+  const jsonData = JSON.stringify(data);
+  const url ="https://us21.api.mailchimp.com/3.0/lists/8339ba7b6a";
+  const options = {
+    method: "POST",
+    auth: "key:838c1cf70b1c98bd779431ff45144997-us21"
+  }
+  const request = https.request(url, options, function(response){
+    if(response.statusCode === 200){
+      res.sendFile(__dirname + "/success.html");
+    }
+    else{
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    })
+
+  })
+  request.write(jsonData);
+  request.end();
+
+});
+//api key
+//838c1cf70b1c98bd779431ff45144997-us21
+
+//list id
+//8339ba7b6a
+app.post("/failure", function(req, res){
+  res.redirect("/")
+})
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log("Server is running");
+})
